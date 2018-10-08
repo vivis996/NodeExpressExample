890dcd47ea643b78537d1266b4498069abd422e5
Daniel Viveros <dviveros90@gmail.com>
2018-10-08 00:22:24 -0500

    Improve error page and added table in users page

--- routes/error.js
+++ routes/error.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('error', { page: 'Error', menuId: 'error' });
+	res.render('error', { page: '500', menuId: 'error', message: 'Error interno' });
 });
 
 router.get('/404', function (req, res, next) {
-	res.render('error', { page: '404', menuId: 'error' });
+	res.render('error', { page: '404', menuId: 'error', message: 'Página no encontrada' });
 });
 
 router.get('/403', function (req, res, next) {
-	res.render('error', { page: '403', menuId: 'error' });
+	res.render('error', { page: '403', menuId: 'error', message: 'Acceso no permitido' });
 });
 
 module.exports = router;
